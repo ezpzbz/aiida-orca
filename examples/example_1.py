db4f4ecc8dee77b386a84cbabc2fcebc0b34f240
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-03-17 09:29:45 +0100

    enabled github actions/ fixed restarts and renamed examples

--- examples/example_1.py
+++ examples/example_1.py
@@ -1,28 +1,32 @@
-"""Run restart numerical Freq Calculation using AiiDA-Orca"""
-
+"""Run simple DFT calculation"""
+import os
 import sys
 import click
 import pytest
-# import pymatgen as mg
+
+import pymatgen as mg
 
 from aiida.engine import run_get_pk
-from aiida.orm import (load_node, Code, Dict)
+from aiida.orm import (load_node, Code, Dict, StructureData)
 from aiida.common import NotExistent
 from aiida.plugins import CalculationFactory
 
 OrcaCalculation = CalculationFactory('orca')  #pylint: disable = invalid-name
 
 
-def example_restart_numfreq(orca_code, submit=True):
-    """Run restart numerical Freq Calculation using AiiDA-Orca"""
+def example_opt_restart(orca_code, opt_calc_pk=None, submit=True):
+    """Run simple DFT calculation"""
 
     # This line is needed for tests only
-    if freq_calc_pk is None:
-        freq_calc_pk = pytest.freq_calc_pk  # pylint: disable=no-member
+    if opt_calc_pk is None:
+        opt_calc_pk = pytest.opt_calc_pk  # pylint: disable=no-member
+
     # structure
-    # Optimized structure
-    # structure = load_node(2229)
-    # hessian = load_node(2230)
+    thisdir = os.path.dirname(os.path.realpath(__file__))
+    xyz_path = os.path.join(thisdir, 'ch4.xyz')
+    structure = StructureData(pymatgen_molecule=mg.Molecule.from_file(xyz_path))
+
+    # parent_calc_folder = load_node(2104)
 
     # parameters
     parameters = Dict(
@@ -36,35 +40,29 @@ def example_restart_numfreq(orca_code, submit=True):
                 # 'pal': {
                 #     'nproc': 2,
                 # },
-                'freq': {
-                    'restart': 'True',
-                    'temp': 273,
-                }
             },
-            'input_kewords': ['RKS', 'BP', 'def2-TZVP', 'RI', 'def2/J'],
-            'extra_input_keywords': ['Grid5', 'NoFinalGrid', 'NumFreq'],
+            'input_keywords': ['B3LYP/G', 'def2-TZVP', 'Opt'],
+            'extra_input_keywords': ['MOREAD'],
         }
     )
 
     # Construct process builder
-
     builder = OrcaCalculation.get_builder()
 
-    builder.structure = load_node(freq_calc_pk).outputs.relaxed_structure
+    builder.structure = structure
     builder.parameters = parameters
     builder.code = orca_code
-    builder.hessian = load_node(freq_calc_pk).outputs.hessian
-
+    builder.parent_calc_folder = load_node(opt_calc_pk).outputs.remote_folder
     builder.metadata.options.resources = {
         'num_machines': 1,
         'num_mpiprocs_per_machine': 1,
     }
     builder.metadata.options.max_wallclock_seconds = 1 * 10 * 60
     if submit:
-        print('Testing ORCA  restart numerical Frequency Calculation...')
+        print('Testing Orca Opt Calculations...')
         res, pk = run_get_pk(builder)
         print('calculation pk: ', pk)
-        print('Enthalpy is :', res['output_parameters'].dict['enthalpy'])
+        print('SCF Energy is :', res['output_parameters'].dict['SCF_energies'])
     else:
         builder.metadata.dry_run = True
         builder.metadata.store_provenance = False
@@ -72,15 +70,16 @@ def example_restart_numfreq(orca_code, submit=True):
 
 @click.command('cli')
 @click.argument('codelabel')
+@click.option('--previous_calc', '-p', required=True, type=int, help='PK of example_0.py calculation')
 @click.option('--submit', is_flag=True, help='Actually submit calculation')
-def cli(codelabel, submit):
+def cli(codelabel, previous_calc, submit):
     """Click interface"""
     try:
         code = Code.get_from_string(codelabel)
     except NotExistent:
         print("The code '{}' does not exist".format(codelabel))
         sys.exit(1)
-    example_restart_numfreq(code, submit)
+    example_opt_restart(code, previous_calc, submit)
 
 
 if __name__ == '__main__':
