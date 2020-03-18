85b9931a7893728a8887fa8d1c1877ef4c300e6e
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-03-18 23:39:11 +0100

    finalized openmpi in actions + minor changes in readme

--- README.md
+++ README.md
@@ -1,13 +1,20 @@
 
 [![Actions Status](https://github.com/pzarabadip/aiida-orca/workflows/Build/badge.svg)](https://github.com/pzarabadip/aiida-orca/actions)
 [![codecov](https://codecov.io/gh/pzarabadip/aiida-orca/branch/master/graph/badge.svg)](https://codecov.io/gh/pzarabadip/aiida-orca)
+[![GitHub license](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/pzarabadip/aiida-orca/blob/master/LICENSE)
+
+Tested with:
+
+[![orca](https://img.shields.io/badge/ORCA-4.2.1-1f425f.svg)](https://orcaforum.kofo.mpg.de/app.php/portal)
+[![openmpi](https://img.shields.io/badge/OpenMPI-2.1.6-1f425f.svg)](https://www.open-mpi.org/)
+
 
 <!-- [![PyPI version](https://badge.fury.io/py/aiida-orca.svg)](https://badge.fury.io/py/aiida-orca) -->
 <!-- [![PyPI pyversions](https://img.shields.io/pypi/pyversions/aiida-orca.svg)](https://pypi.python.org/pypi/aiida-orca/) -->
 
 <!-- [![DOI](https://zenodo.org/badge/201230518.svg)](https://zenodo.org/badge/latestdoi/201230518) -->
 
-[![GitHub license](https://img.shields.io/badge/License-MIT-blue.svg)](https://github.com/pzarabadip/aiida-orca/blob/master/LICENSE)
+
 
 **DISCLAIMER**: Under heavy development!
 
@@ -30,4 +37,4 @@ MIT
 # Acknowledgment
 I would like to thank the funding received from the European Union’s Horizon 2020 research and innovation programme under the Marie Skłodowska-Curie Actions and cofinancing by the South Moravian Region under agreement 665860. This software reflects only the authors’ view and the EU is not responsible for any use that may be made of the information it contains.
 
-![aiida-orca](ackn_logo.png)
+![aiida-orca](miscellaneous/ackn_logo.png)
--- conftest.py
+++ conftest.py
@@ -3,10 +3,17 @@ For pytest initialise a test database and profile
 """
 import os
 import pytest
+
 pytest_plugins = ['aiida.manage.tests.pytest_fixtures']  # pylint: disable=invalid-name
 
 thisdir = os.path.dirname(os.path.realpath(__file__))  # pylint: disable=invalid-name
 prepend_text = 'source ' + str(os.path.join(thisdir, '.github', 'setup.sh'))  # pylint: disable=invalid-name
+
+# Example of how to define your own fixture
+# @pytest.fixture(scope='function', autouse=True)
+# def clear_database_auto(clear_database):
+#     """Automatically clear database in between tests."""
+#     pass
 
 
 @pytest.fixture(scope='function')
@@ -14,31 +21,4 @@ def orca_code(aiida_local_code_factory):  # pylint: disable=unused-argument
     return aiida_local_code_factory('orca', 'orca', prepend_text=prepend_text)
 
 
-# @pytest.fixture(scope='function')
-# def orca_code(aiida_local_code_factory):  # pylint: disable=unused-argument
-#     return aiida_local_code_factory('orca', 'orca',
-# prepend_text='source /home/runner/work/aiida-orca/aiida-orca/.github/setup.sh')
-
-# # Clearing database between tests
-# @pytest.fixture(scope='function', autouse=True)
-# def clear_database_auto(clear_database):
-#     """Automatically clear database in between tests."""
-#     pass
-
-# Setting up computer and code.
-# @pytest.fixture(scope='function')
-# def orca_code(aiida_local_code_factory):  # pylint: disable=unused-argument
-#     return aiida_local_code_factory(
-#         'orca',
-#         'orca',
-#         prepend_text=
-#         'export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/orca_4_2_1_linux_x86-64_shared_openmpi216'
-#     )
-
-# ompi_bin = 'export PATH=/home/runner/work/aiida-orca/aiida-orca/ompi216/bin:$PATH'  # pylint: disable=invalid-name
-# ompi_lib = 'export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/ompi216/lib:$LD_LIBRARY_PATH'  # pylint: disable=invalid-name
-# orca_bin = 'export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/orca421:$LD_LIBRARY_PATH'  # pylint: disable=invalid-name
-
-# @pytest.fixture(scope='function')
-# def orca_code(aiida_local_code_factory):  # pylint: disable=unused-argument
-#     return aiida_local_code_factory('orca', 'orca', prepend_text=ompi_lib + '\n' + ompi_lib + '\n' + orca_bin + '\n')
+#EOF
