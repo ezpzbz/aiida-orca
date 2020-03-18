0b56a525a0462d0312333e8c0b7a1efa8dd0fd09
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-03-18 22:23:59 +0100

    test orca run with openmpi

---
+++ .github/setup.sh
@@ -0,0 +1,4 @@
+# Setting up environment variables
+export PATH=/home/runner/work/aiida-orca/aiida-orca/ompi216/bin:$PATH
+export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/ompi216/lib:${LD_LIBRARY_PATH}
+export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/orca421:${LD_LIBRARY_PATH}
