ea06e4b9df1a0e38bbcbeef584f212966db88a1b
Pezhman Zarabadi-Poor <pzarabadip@gmail.com>
2020-03-19 00:07:43 +0100

    Enable openmpi (#20)
    * enabling openmpi
    
    * enabling openmpi --> fix issue with code
    
    * test orca run with openmpi
    
    * test orca run with openmpi
    
    * test orca run with openmpi
    
    * test orca run with openmpi
    
    * test orca run with openmpi
    
    * test orca run with openmpi
    
    * finalized openmpi in actions + minor changes in readme
    
    * removed python 3.8 build for now

--- conftest.py
+++ conftest.py
@@ -1,15 +1,24 @@
 """
 For pytest initialise a test database and profile
 """
+import os
 import pytest
+
 pytest_plugins = ['aiida.manage.tests.pytest_fixtures']  # pylint: disable=invalid-name
+
+thisdir = os.path.dirname(os.path.realpath(__file__))  # pylint: disable=invalid-name
+prepend_text = 'source ' + str(os.path.join(thisdir, '.github', 'setup.sh'))  # pylint: disable=invalid-name
+
+# Example of how to define your own fixture
+# @pytest.fixture(scope='function', autouse=True)
+# def clear_database_auto(clear_database):
+#     """Automatically clear database in between tests."""
+#     pass
 
 
 @pytest.fixture(scope='function')
 def orca_code(aiida_local_code_factory):  # pylint: disable=unused-argument
-    return aiida_local_code_factory(
-        'orca',
-        'orca',
-        prepend_text=
-        'export LD_LIBRARY_PATH=/home/runner/work/aiida-orca/aiida-orca/orca_4_2_1_linux_x86-64_shared_openmpi216'
-    )
+    return aiida_local_code_factory('orca', 'orca', prepend_text=prepend_text)
+
+
+#EOF
