3a79a3ae530fb9e1f5af24cdf80e8226847dfde9
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2023-02-16 19:50:02 +0000

    Handle unrecoverable errors in OrcaBaseWorkchain (#49)
    Before this change, the OrcaBaseWorkchain would try to restart the Calcjob for all errors,
    even though all currently known errors are unrecoverable. 
     - CalcJob returns error below 300. This error comes outside of this plugin
       so the OrcaBaseWorkChain returns UNKNOWN_UNRECOVERABLE_ERROR.
     - CalcJob returns error between 300 and 399. These errors should come from aiida-orca
       and are not recoverable, the workchain returns ERROR_AIIDA_ORCA_UNRECOVERABLE_FAILURE
     - In the future, if we have a recoverable error the CalcJob should return error > 400
       and a new handler should be added.
     - I removed the `NO_RETRIEVED_FOLDER`, it's better to raise in this case.

--- aiida_orca/workchains/base.py
+++ aiida_orca/workchains/base.py
@@ -2,10 +2,10 @@
 """Base work chain to run an ORCA calculation"""
 
 from aiida.common import AttributeDict
-from aiida.engine import BaseRestartWorkChain, while_
+from aiida.engine import BaseRestartWorkChain, ProcessHandlerReport, process_handler, while_
 from aiida.plugins import CalculationFactory
 
-OrcaCalculation = CalculationFactory('orca.orca')  # pylint: disable=invalid-name
+OrcaCalculation = CalculationFactory('orca.orca')
 
 
 class OrcaBaseWorkChain(BaseRestartWorkChain):
@@ -26,6 +26,16 @@ class OrcaBaseWorkChain(BaseRestartWorkChain):
             cls.results,
         )
         spec.expose_outputs(OrcaCalculation)
+        spec.exit_code(
+            300,
+            'ERROR_UNKNOWN_UNRECOVERABLE_FAILURE',
+            message='The calculation failed with an unidentified unrecoverable error.'
+        )
+        spec.exit_code(
+            301,
+            'ERROR_AIIDA_ORCA_UNRECOVERABLE_FAILURE',
+            message='The calculation failed with an unrecoverable error coming from aiida-orca.'
+        )
 
     def setup(self):
         """Call the `setup` of the `BaseRestartWorkChain` and then create the inputs dictionary in `self.ctx.inputs`.
@@ -33,7 +43,6 @@ class OrcaBaseWorkChain(BaseRestartWorkChain):
         internal loop."""
 
         super().setup()
-
         self.ctx.inputs = AttributeDict(self.exposed_inputs(OrcaCalculation, 'orca'))
 
     def report_error_handled(self, calculation, action):
@@ -42,9 +51,29 @@ class OrcaBaseWorkChain(BaseRestartWorkChain):
         :param calculation: the failed calculation node
         :param action: a string message with the action taken"""
 
-        arguments = [calculation.process_label, calculation.pk, calculation.exit_status, calculation.exit_message]
-        self.report('{}<{}> failed with exit status {}: {}'.format(*arguments))
-        self.report('Action taken: {}'.format(action))
+        self.report(
+            f'{calculation.process_label}<{calculation.pk}> failed with exit status '
+            f'{calculation.exit_status}: {calculation.exit_message}'
+        )
+        self.report(f'Action taken: {action}')
 
+    @process_handler()
+    def handle_known_unrecoverable_failure(self, calculation):
+        """Handle exit status between 300-399.
 
-#EOF
+           These errors should come from aiida-orca plugin and are not recoverable.
+        """
+        if calculation.is_failed and calculation.exit_status < 400 and calculation.exit_status > 299:
+            self.report_error_handled(calculation, 'unrecoverable aiida-orca error, aborting...')
+            return ProcessHandlerReport(True, self.exit_codes.ERROR_AIIDA_ORCA_UNRECOVERABLE_FAILURE)  # pylint: disable=no-member
+
+    @process_handler()
+    def handle_unknown_unrecoverable_failure(self, calculation):
+        """Handle exit status between 1-299
+
+           These errors come from outside of aiida-orca plugin.
+           For now we suppose that all of these are unrecoverable.
+        """
+        if calculation.is_failed and calculation.exit_status < 300:
+            self.report_error_handled(calculation, 'unknown unrecoverable error, aborting...')
+            return ProcessHandlerReport(True, self.exit_codes.ERROR_UNKNOWN_UNRECOVERABLE_FAILURE)  # pylint: disable=no-member
